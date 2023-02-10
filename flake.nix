256ddb2620ae3431a1495a26202e9b605f7b84ed
Michon van Dooren <me@maienm.com>
2023-02-10 20:48:52 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+    poetry2nix.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      p2n = poetry2nix.legacyPackages.${system};
+      common = {
+        projectDir = self;
+      };
+    in
+    {
+      packages = rec {
+        yaml_format = p2n.mkPoetryApplication common;
+        default = yaml_format;
+      };
+
+      devShells.default = (p2n.mkPoetryEnv common).env.overrideAttrs (prev: {
+        buildInputs = [
+          pkgs.python310
+          pkgs.poetry
+        ];
+      });
+    }
+  );
+}
